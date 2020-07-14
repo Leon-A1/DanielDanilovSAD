467bc0c5400a8058c934a39dd6cac13ce1ba2aa0
Leon-A1 <leonaviev@gmail.com>
2020-07-14 20:35:40 +0300

    design fix

--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -12,17 +12,23 @@ export default function SlidingResponsiveNavbar() {
     document.getElementById("side-menu").style.width = "250px";
     document.getElementById("navBarLogo").style.opacity = "30%";
     document.getElementById("topNavbarItems").style.opacity = "30%";
+    document.getElementById("home-small-nav-item").style.opacity = "1";
+    document.getElementById("profile-small-nav-item").style.opacity = "1";
+    document.getElementById("contact-small-nav-item").style.opacity = "1";
   }
 
   function closeSlideMenu() {
+    document.getElementById("home-small-nav-item").style.opacity = "0";
+    document.getElementById("profile-small-nav-item").style.opacity = "0";
+    document.getElementById("contact-small-nav-item").style.opacity = "0";
+
     document.getElementById("side-menu").style.width = "0";
     document.getElementById("topNavbarItems").style.opacity = "100%";
     document.getElementById("navBarLogo").style.opacity = "100%";
   }
 
   return (
-    // <div style={{ zIndex: 1000, position: "absolute", margin: 100 }}>
-    <div>
+    <div style={{ zIndex: 10000 }}>
       <nav className="navbar">
         <span className="open-slide">
           <button onClick={openSlideMenu}>
@@ -33,43 +39,48 @@ export default function SlidingResponsiveNavbar() {
             </svg>
           </button>
         </span>
-        <nav style={{ height: "100vh", zIndex: 1000 }}></nav>
+        <nav style={{}}></nav>
 
-        <ul id="topNavbarItems" className="navbar-nav">
-          <li className="nav-item">
-            <NavLink to="/" exact className="nav-link text-white">
+        <ul id="topNavbarItems">
+          <li>
+            <NavLink to="/" exact>
               Home
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/companyprofile" className="nav-link text-white">
-              Company Profile
-            </NavLink>
+          <li>
+            <NavLink to="/companyprofile">Company Profile</NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/getintouch" className="nav-link text-white">
-              צור קשר
-            </NavLink>
+          <li>
+            <NavLink to="/getintouch">צור קשר</NavLink>
           </li>
         </ul>
         <img id="navBarLogo" className="navBarLogo" src={logo} alt="logo" />
       </nav>
-
       <div id="side-menu" className="side-nav">
-        {/* <button className="btn-close" onclick={closeSlideWindow}> */}
+        <ul>
+          <li>
+            {" "}
+            <a id="home-small-nav-item" href="/">
+              עמוד הבית
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a id="profile-small-nav-item" href="/companyprofile">
+              פרופיל חברה
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a id="contact-small-nav-item" href="/getintouch">
+              צור קשר
+            </a>
+          </li>
+        </ul>
+
         <button className="btn-close" onClick={closeSlideMenu}>
           &times;
         </button>
-
-        <a id="smaller-display-side-navbar-items" href="/">
-          עמוד הבית
-        </a>
-        <a id="smaller-display-side-navbar-items" href="/companyprofile">
-          פרופיל חברה
-        </a>
-        <a id="smaller-display-side-navbar-items" href="/getintouch">
-          צור קשר
-        </a>
       </div>
     </div>
   );
